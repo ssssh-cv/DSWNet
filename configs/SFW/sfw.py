14329961f304dfeb8d389759e4dfb0a0ace63fa4
ssssh-cv <JrogelioBaruch24@gmail.com>
2025-12-29 17:35:15 +0800

    sfw.py

--- configs/SFW/sfw.py
+++ configs/SFW/sfw.py
@@ -2,22 +2,19 @@ _base_ = [
     '../_base_/default_runtime.py',
 ]
 
-# experiment_name = 'block6_reEMA_bs12'
 experiment_name = 'sfw'
 
 work_dir = f'./work_dirs/{experiment_name}'
 save_dir = './work_dirs/'
 
-# model settings
 model = dict(
     type='BaseUWModel',
     generator=dict(type='SFWnet'),
-    pixel_loss=dict(type='L1Loss', loss_weight=4.0, reduction='mean'), #5.0
-    # pixel_loss=dict(type='MSELoss', loss_weight=1.0, reduction='mean'),
+    pixel_loss=dict(type='L1Loss', loss_weight=4.0, reduction='mean'),
 
-    ssim_loss=dict(type='SSIMLoss', loss_weight=3.0), #3.0
+    ssim_loss=dict(type='SSIMLoss', loss_weight=3.0),
     contrastive_loss=dict(
-        type='ContrastiveLoss1', #ContrastiveLoss
+        type='ContrastiveLoss1',
         layer_weights={
             '1': 1.0 / 32,
             '6': 1.0 / 16,
@@ -79,12 +76,10 @@ val_pipeline = [
         type='Resize',
         keys=['img', 'gt'],
         scale=(256, 256)),
-    # dict(type='SetValues', dictionary=dict(scale=1)),
-    # dict(type='PairedRandomCrop', gt_patch_size=256),
     dict(type='PackInputs')
 ]
 
-# dataset settings
+
 train_dataloader = dict(
     num_workers=4,
     batch_size=4,
@@ -93,10 +88,8 @@ train_dataloader = dict(
     dataset=dict(
         type='BasicImageDataset',
         metainfo=dict(dataset_type='mix', task_name='train'),
-        # data_root='../data/train',
 
-        data_root='E:/pycharm/mmagic-1.2.0/datasets/train',  # UIEB 800png
-        # data_root='C:/Users/pc/Desktop/dataset/train', # 自建 1600png
+        data_root='put your data path',
 
         data_prefix=dict(img='lq', gt='gt'),
         pipeline=train_pipeline))
@@ -110,8 +103,7 @@ val_dataloader = dict(
         type='BasicImageDataset',
         metainfo=dict(dataset_type='real', task_name='val'),
 
-        data_root='E:/pycharm/mmagic-1.2.0/datasets/val',  # UIEB 90png
-        # data_root='C:/Users/pc/Desktop/dataset/val', # 自建 190png
+        data_root='put your data path',
 
         data_prefix=dict(img='lq', gt='gt'),
         pipeline=val_pipeline))
@@ -125,25 +117,11 @@ train_cfg = dict(
     type='IterBasedTrainLoop', max_iters=40000, val_interval=500)
 val_cfg = dict(type='MultiValLoop')
 
-# optimizer
 optim_wrapper = dict(
     constructor='DefaultOptimWrapperConstructor',
     type='OptimWrapper',
     optimizer=dict(type='Adam', lr=1e-3, betas=(0.9, 0.999)))
 
-# learning policy
-# param_scheduler = [
-#     dict(type='LinearLR',
-#         start_factor=0.0001,
-#         by_epoch=False,
-#         begin=0,
-#         end=1000),
-#     dict(type='CosineAnnealingLR',
-#         T_max=35000,
-#         by_epoch=False,
-#         begin=1000,
-#         end=40000)
-# ]
 
 default_hooks = dict(
     checkpoint=dict(
@@ -158,7 +136,6 @@ default_hooks = dict(
     param_scheduler=dict(type='ParamSchedulerHook'),
     sampler_seed=dict(type='DistSamplerSeedHook'))
 
-# vis_backends = [dict(type='LocalVisBackend'), dict(type='WandbVisBackend')]
 vis_backends = [dict(type='LocalVisBackend')]
 visualizer = dict(
     type='ConcatImageVisualizer',
