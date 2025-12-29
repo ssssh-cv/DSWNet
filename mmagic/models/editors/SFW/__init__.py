f27ff6a11a190145c6f57432c0d9a2a6c8f702e9
ssssh-cv <JrogelioBaruch24@gmail.com>
2025-12-29 17:37:42 +0800

    __init__.py

--- mmagic/models/editors/SFW/__init__.py
+++ mmagic/models/editors/SFW/__init__.py
@@ -1,12 +1,6 @@
 from .sfw import SFWnet
-from .nocoda import Nocoda
-from .nofde import Nofde
-from .norefine import Norefine
-from .baseline import Baseline
-from .WDM import WDM
-from .dskip import Dskip
-from .sfw_diff import SFWdiff
+
 
 __all__ = [
-    'SFWnet', 'Nocoda', 'Nofde', 'Norefine', 'Baseline', 'WDM', 'Dskip', 'SFWdiff',
+    'SFWnet',
 ]
